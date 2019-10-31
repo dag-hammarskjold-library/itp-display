12b26ff69b0fc001dce53b3faae571b11533b7cc
Eric Attere <Eric.Attere@UNHQ.UN.ORG>
2019-10-31 12:03:14 -0400

    new files added

---
+++ app.py
@@ -0,0 +1,28 @@
+### imports
+
+from flask import Flask,jsonify,render_template
+from pymongo import MongoClient
+from config import DATABASE_CONSTRING
+
+
+app = Flask(__name__)
+
+# Route Definition
+
+@app.route("/")
+@app.route("/index")
+def index():
+    myMongoURI=DATABASE_CONSTRING
+    myClient = MongoClient(myMongoURI)
+    myDatabase=myClient.undlFiles
+    myCollection=myDatabase['itp_sample_output']
+    myRecords=myCollection.find()
+    records=myRecords
+    return render_template("index.html",records=records)
+
+# Main program
+
+if __name__ == '__main__':
+  
+    app.debug=True
+    app.run()
