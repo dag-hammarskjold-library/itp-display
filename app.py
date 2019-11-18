38757ac7c52522341d39e5e2f74cd604b4723e0e
Eric Attere <Eric.Attere@UNHQ.UN.ORG>
2019-11-18 14:42:17 -0500

    portail added

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
 ### imports
 
-from flask import Flask,jsonify,render_template
+from flask import Flask,jsonify,render_template,url_for
 from pymongo import MongoClient
 from config import DATABASE_CONSTRING,URL_PREFIX
-from data import myData
 
 
 app = Flask(__name__)
 test="just a test 02"
 
-# Route Definition
+myMongoURI=DATABASE_CONSTRING
+myClient = MongoClient(myMongoURI)
+myDatabase=myClient.undlFiles
+myCollection=myDatabase['itp_sample_output']
 
+
+# Route Definition
 @app.route("/")
-@app.route("/index")
-def index():
-    myMongoURI=DATABASE_CONSTRING
-    myClient = MongoClient(myMongoURI)
-    myDatabase=myClient.undlFiles
-    myCollection=myDatabase['itp_sample_output']
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+@app.route("/itpp_itpsor")
+def itpp_itpsor():
     myRecords=myCollection.find()
     records=myRecords
-    number=myCollection.find().count()
-    sideA=round(number/2)+1
-    sideB=number-sideA
-    return render_template("index.html",records=records,number=number,sideA=sideA,sideB=sideB)
+    return render_template("itpsor.html",records=records)
+
+@app.route("/itpp_itpitsp")
+def itpp_itpitsp():
+    myRecords=myCollection.find({'section': "itpitsp"},{'itshead':1, 'itssubhead':1, 'itsentry':1})
+    records=myRecords
+    return render_template("itpitsp.html",records=records,myURL=URL_PREFIX)
+
+@app.route("/itpp_itpitsc")
+def itpp_itpitsc():
+    myRecords=myCollection.find({'section': "itpitsc"}, {'itshead':1, 'itssubhead':1, 'itsentry':1, 'docsymbol': 1})
+    records=myRecords
+    return render_template("itpitsc.html",records=records,myURL=URL_PREFIX)
 
 
-@app.route("/indextospeeches")
-def indexToSpeeches():
-    return render_template("indextospeeches.html",records=myData,myURL=URL_PREFIX)
+@app.route("/itpp_itpitss")
+def itpp_itpitss():
+    myRecords=myCollection.find({'section': "itpitss"}, {'itshead':1, 'itssubhead':1, 'itsentry':1, 'docsymbol': 1})
+    records=myRecords
+    return render_template("itpitss.html",records=records,myURL=URL_PREFIX)
 
 # Main program
 
